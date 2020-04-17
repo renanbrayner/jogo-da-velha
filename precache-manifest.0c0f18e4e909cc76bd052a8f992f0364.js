6b841f8620291250931d64ea95b53018ffc7c0f1
Renan Brayner <renanbrayner@gmail.com>
2020-04-17 18:03:37 -0300

    Updates

---
+++ precache-manifest.0c0f18e4e909cc76bd052a8f992f0364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e67dd03c6238fb0ce77d865392fc505",
+    "url": "/jogo-da-velha/index.html"
+  },
+  {
+    "revision": "2aca2fd847da6351755f",
+    "url": "/jogo-da-velha/static/css/main.7b044988.chunk.css"
+  },
+  {
+    "revision": "2d0cc957a5c3ce4627e5",
+    "url": "/jogo-da-velha/static/js/2.ed73a67e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jogo-da-velha/static/js/2.ed73a67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aca2fd847da6351755f",
+    "url": "/jogo-da-velha/static/js/main.f515377d.chunk.js"
+  },
+  {
+    "revision": "29372fa124240fa98e5d",
+    "url": "/jogo-da-velha/static/js/runtime-main.b00cbf09.js"
+  }
+]);
